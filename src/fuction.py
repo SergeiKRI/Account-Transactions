2d5cabc3cbb5fe4dabf504b68ffb07680333a7e1
SerKRI <SK@main.ru>
2024-01-16 02:29:30 +0400

    work version

--- src/fuction.py
+++ src/fuction.py
@@ -15,7 +15,7 @@ def is_exect(lst):
     '''
     ex_lst = []
     for s in lst:
-        if s["state"] == "EXECUTED":
+        if s.get("state")== "EXECUTED":
             ex_lst.append(s)
     return ex_lst
 
@@ -25,22 +25,49 @@ def sort_data(lst):
     :param lst: неотсортированный список
     :return: список по дате операции
     '''
+    app_data = []
+    for data in lst:
+        if data.get('date'):
+            app_data.append(data)
 
-    return sorted(lst, key=lambda data: data.get("date"))
+    return sorted(app_data, key=lambda x: x.get("date"), reverse=True)
 
+def form_data(date):
+    '''
+    Дата перевода представлена в формате ДД.ММ.ГГГГ (пример: 14.10.2018)
+    :param date: дата в формате ГГГГ-ММ-ДД
+    :return:
+    '''
+    mod_date = date.split('-')
+    return f'{mod_date[2][:2]}.{mod_date[1]}.{mod_date[0]}'
 
-if __name__=='__main__':
-    lst = load_operations()
-    sort_data(lst)
+def mask_card(number):
+    """
+Номер карты в формате  XXXX XX** **** XXXX
+Номер счета в формате  **XXXX
+    :param number: str
+    :return:
+    """
+    name_card = number.split(' ')
+    if name_card[0] == 'Счет':
+        return 'Счет **' + name_card[-1][-4:]
 
+    else:
+        form_card = name_card[-1][:6]+'******'+name_card[-1][-4:]
+        # разбивает номер по 4 цифры
+        num_ = [form_card[i:i+4] for i in range(0, len(form_card), 4)]
 
+        return ' '.join(name_card[:-1] + num_)
 
-
-
-
-
-
-
-
-
-
+def form_modif(dict_):
+    '''
+    Выводит в нужном формате
+14.10.2018 Перевод организации
+Visa Platinum 7000 79** **** 6361 -> Счет **9638
+82771.72 руб.
+    :param dict_: dict
+    :return: str
+    '''
+    return (f'{form_data(dict_["date"])} {dict_["description"]}\n'
+            f'{mask_card(dict_.get("from"," "))} -> {mask_card(dict_["to"])}\n'
+            f'{dict_["operationAmount"]["amount"]} {dict_["operationAmount"]["currency"]["name"]}\n')
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from fuction import *
+
+if __name__=='__main__':
+    print(form_modif(is_exect(load_operations())[1]))
